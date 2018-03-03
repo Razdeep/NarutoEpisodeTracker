f14431c7d5c618d94f6a80342965e83e0004938a
razdeep <rrajdeeproychowdhury@gmail.com>
2018-03-03 16:56:39 +0530

    UI Modified

--- app/src/main/java/naruto/rajdeep/com/narutoepisodetracker/MainActivity.java
+++ app/src/main/java/naruto/rajdeep/com/narutoepisodetracker/MainActivity.java
@@ -2,12 +2,21 @@ package naruto.rajdeep.com.narutoepisodetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView numberField;
+    Button updateBtn,rectifyBtn,setBtn,aboutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numberField=findViewById(R.id.number_field);
+        updateBtn=findViewById(R.id.update_btn);
+        rectifyBtn=findViewById(R.id.rectify_btn);
+        aboutBtn=findViewById(R.id.about_btn);
+
     }
+
 }
